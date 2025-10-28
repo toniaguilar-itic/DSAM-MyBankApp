829a44c0f8cf76d7842e7eff72e45fe3bd9674fc
toniaguilar <toni.aguilar@iticbcn.cat>
2025-10-28 15:10:51 +0100

    Tres activities

--- app/src/main/java/cat/iticbcn/mybank/LoginActivity.kt
+++ app/src/main/java/cat/iticbcn/mybank/LoginActivity.kt
@@ -5,40 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.button.MaterialButton
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var btnContinuar : MaterialButton
-
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        initComponents()
-        initListeners()
     }
-    private fun initComponents() {
-        btnContinuar = findViewById<MaterialButton>(R.id.btnContinuar)
-    }
-
-    private fun initListeners() {
-        btnContinuar.setOnClickListener {
-            goToLoginActivity()
-        }
-    }
-
-    private fun goToLoginActivity() {
-        val intent = android.content.Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-    }
-
 }
-
-
-
-
