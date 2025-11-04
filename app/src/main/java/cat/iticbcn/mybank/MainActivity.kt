3e8b0ec716fdcb64314460ffbbb263deb8bdefab
toniaguilar <toni.aguilar@iticbcn.cat>
2025-11-04 19:34:57 +0100

    Activitat inicial

--- app/src/main/java/cat/iticbcn/mybank/MainActivity.kt
+++ app/src/main/java/cat/iticbcn/mybank/MainActivity.kt
@@ -1,6 +1,9 @@
 package cat.iticbcn.mybank
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +12,9 @@ import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnContinuar : MaterialButton
+    private lateinit var ivBack: ImageView
+    private lateinit var etDocument: EditText
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,21 +29,65 @@ class MainActivity : AppCompatActivity() {
         initListeners()
     }
     private fun initComponents() {
+        ivBack = findViewById<ImageView>(R.id.ivBack)
         btnContinuar = findViewById<MaterialButton>(R.id.btnContinuar)
+        etDocument = findViewById<EditText>(R.id.etDocument)
     }
 
     private fun initListeners() {
+        ivBack.setOnClickListener {
+            terminarAplicacio()
+
+        }
+
         btnContinuar.setOnClickListener {
             goToLoginActivity()
         }
+
+        etDocument.addTextChangedListener(object : android.text.TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: android.text.Editable?) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (etDocument.length() == 9) {
+                    validarDNI()}
+            }
+        })
     }
 
     private fun goToLoginActivity() {
         val intent = android.content.Intent(this, LoginActivity::class.java)
         startActivity(intent)
     }
+    private fun terminarAplicacio() {
+        AlertDialog.Builder(this)
+            .setTitle("Sortir de l'aplicació")
+            .setMessage("Segur que vols sortir de l'aplicació?")
+            .setPositiveButton("Sí") { _, _ ->
+                finish()
+            }
+            .setNegativeButton("No", null)
+            .setCancelable(true)
+            .show()
+    }
+    private fun validarDNI() {
+        TODO("Not yet implemented")
+    }
+
+    private fun isValidDNI(dni: String): Boolean {
+        var resultat = false
+        if (dni.length != 9) {
+            resultat = false
+        } else {
+
+        }
+
+
+        return resultat
+    }
 
 }
+
+
 
 
 
